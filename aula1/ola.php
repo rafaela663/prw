f8928eb6a90b4c43fc5e88f927a397899fad58c7
rafaela663 <rafaelarodrigues8055@gmail.com>
2023-02-14 11:00:52 -0300

    Meu primeiro programa php

---
+++ aula1/ola.php
@@ -0,0 +1,3 @@
+<?php 
+    echo "<h1>OLÁ MUNDO</h1>";
+?>
---
+++ aula1/ola2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- aqui é o codigo HTML --> 
+    <h1>Exemplo Olá mundo</h1>
+    <?php
+      //Aqui é o codigo php
+      echo "<h1> Olá Mauricio</h1>"
+    ?>
+</body>
+</html>
